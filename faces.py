685cbfc77c281fa3fbc39d88b686b352bfad466d
DrPRun <83342182+DrPRun@users.noreply.github.com>
2021-05-16 01:20:54 +0300

    added class fasese. Now we can work with list of fases as in previeus version

--- faces.py
+++ faces.py
@@ -9,5 +9,39 @@ class Fases():
             return self.a
         elif (i==1):
             return  self.b
-        else:
+        elif (i==2):
             return self.c
+    def __index__(self):
+        return print('hello')
+
+
+class Changeable:
+    def __init__(self, color):
+        self.color = color
+
+    def __call__(self, newcolor):
+        self.color = newcolor
+
+    def __str__(self): # Метод, который возвращает строку
+        return "%s" % self.color
+
+
+class fases(object):
+
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.storage = {}
+
+    def __setitem__(self, key, value):
+        self.storage[key] = value
+    def __getitem__(self, key):
+        if (key == 0):
+            return  self.a
+        elif (key == 1):
+            return  self.b
+        elif (key == 2):
+            return  self.c
+        else:
+            return None
--- main.py
+++ main.py
@@ -2,29 +2,42 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+   # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# print_hi('Dr_run')
+#
+# print_hi('Ruslan')
+# x = input('введите число:')
+# print_hi(x)
+#
+# def split_string():
+#     string = input('Введите строку: ')
+#     return print(string.split())
+# split_string()
+# def ne_test_function():
+#     print('test function for git')
+#     return
+# ne_test_function()
 
+from faces import Fases
+from faces import  Changeable
+from faces import fases
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# all_fases = [MyContainer(1,3,7),MyContainer(3,12,1)]
+# print('номер вершины из списка граней', all_fases[0].cf(2))
+# print('номер из отдельно созданной грани', granm.cf(0))
+# print(all_fases[2][2])
+# print('попытка вывести номер вершины грани  из строки:', granm(0)[1])
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print_hi('Dr_run')
-
-print_hi('Ruslan')
-x = input('введите число:')
-print_hi(x)
-
-def split_string():
-    string = input('Введите строку: ')
-    return print(string.split())
-split_string()
-def ne_test_function():
-    print('test function for git')
-    return
-ne_test_function()
+Fas = fases(1,2,3)
+# print(Fas[0])
+print(Fas[0])
+list_fases = (fases(1,2,3),fases(4,5,6))
+print(list_fases[1][8])
